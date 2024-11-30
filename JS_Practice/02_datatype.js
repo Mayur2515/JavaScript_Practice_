0601573b2713ff54187e0fa9a26f08e88fecd20c
Mayur2515 <mayur.zagade.2511@gmail.com>
2024-11-30 09:39:23 +0000

    datatype

---
+++ JS_Practice/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+let name="mayur"
+let age=78
+let islogout=false
+
+console.log(typeof(name));
+console.log(typeof(age))
+console.log(typeof(islogout));
+
+// datatype 8
+// number
+//bigint
+//String =>""
+//boolean => false/true
+//null  => standalone value
+//undefined  => canot assine any value
+// symbol =>when we find uniquness then we shall go to the symbol 
+
+// these are premetive datatype
+
+// object
+
+
