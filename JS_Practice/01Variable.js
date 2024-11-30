6d0b80d6570fa9af13c80235ee442763950db5b6
Mayur2515 <mayur.zagade.2511@gmail.com>
2024-11-30 11:07:57 +0000

    Convertion

---
+++ JS_Practice/01Variable.js
@@ -0,0 +1,10 @@
+const accountID=656267
+let accountEmail="mayur@gmail"
+var accountPass="12334"
+accountCity="pune"
+
+
+accountEmail="sam@gmail.com"
+accountPass="1234"
+
+console.table([accountID,accountEmail,accountPass,accountCity])
---
+++ JS_Practice/ConversionOpration.js
@@ -0,0 +1,19 @@
+
+
+// let score=undefined
+
+// console.log(typeof "score");
+
+// let valueInNo=Number(score)
+
+// console.log(valueInNo);
+// console.log(typeof valueInNo);
+
+let islogin="jjjj";
+
+let isbooleanin=Boolean(islogin)
+
+console.log( isbooleanin);
+// 1 => true: 0 =>false
+//"" => false
+//"fff"=>true
